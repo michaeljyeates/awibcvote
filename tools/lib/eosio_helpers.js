3d5b17083690f2c6e061ea3ea4f4732ca6795988
Michael Yeates <michaeljyeates@gmail.com>
2023-05-31 11:58:15 +0100

    Add transactionFinal helper

--- tools/lib/eosio_helpers.js
+++ tools/lib/eosio_helpers.js
@@ -12,5 +12,21 @@ const eosioObj = (chain_name) => {
     return api;
 }
 
+const transactionFinal = async (chain_name, tx_id) => {
+    console.log(`Checking tx status for ${tx_id}`);
+    const chain_data = chainData(chain_name);
 
-module.exports = { eosioObj };
+    const url = `${chain_data.nodeUrl}/v1/chain/get_transaction_status`;
+    const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            id: tx_id
+        })
+    });
+    const json = await res.json();
+
+    return json.state === 'IRREVERSIBLE';
+}
+
+
+module.exports = { eosioObj, transactionFinal };
